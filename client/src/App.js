2a1830ecb81d2ee1a1cfc11ff370ecd2532b6847
Angela-Inniss <angela.inniss@hotmail.co.uk>
2022-01-31 12:21:03 +0000

    fix react-select issue add script

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import facebook from "./icons/facebookIcon";
+import selectDropDownFilter from "./components/selectDropdown";
+// import Select from 'react-select'
+import Select, { components } from "react-select";
+// Add this in your component file
 
 // SERVICES THAT CALL OUR API ENDPOINTS
 import { getAllSalons, getSalonsByLocation } from "./services/salonService";
+
 
 function App() {
   const [salons, setSalons] = useState(null);
@@ -17,49 +22,59 @@ function App() {
         setSalons(response);
       }
     }
-
     getSalons();
   }, [salons]);
 
+  // <Select options={options} />
   const renderSalon = (salon) => {
     return (
-      <li key={salon._id}>
-        <h3>
-          {`${salon.name} 
+      <>
+        <li key={salon._id}>
+          <h3>
+            {`${salon.name} 
           ${salon.location}`}
-        </h3>
-        <p>{salon.description}</p>
+          </h3>
+          <p>{salon.description}</p>
 
-        <p>{salon.services.map((service => <p>{service}</p>))}</p>
+          <p>{salon.services.map((service => <p>{service}</p>))}</p>
 
-        <p>{salon.socials.map((social) => {
-          console.log(social)
-          const { facebook, instagram, twitter } = social
-          if (social.facebook) {
-            return <p> <a href={facebook}>facebook</a></p>
-          }
-          if (social.instagram) {
-            return <p><a href={instagram}>Instagram</a></p>
-          }
-          if (social.twitter) {
-            return <p><a href={twitter}>twitter</a></p>
-          }
-        })}</p>
+          <p>{salon.socials.map((social) => {
+            console.log(social)
+            const { facebook, instagram, twitter } = social
+            if (social.facebook) {
+              return <p> <a href={facebook}>facebook</a></p>
+            }
+            if (social.instagram) {
+              return <p><a href={instagram}>Instagram</a></p>
+            }
+            if (social.twitter) {
+              return <p><a href={twitter}>twitter</a></p>
+            }
+          })}</p>
 
-      </li >
+        </li >
+      </>
     );
   };
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
 
   return (
-    <div>
-      <ul>
-        {salons && salons.length > 0 ? (
-          salons.map((salon) => renderSalon(salon))
-        ) : (
-          <p>No salons found</p>
-        )}
-      </ul>
-    </div>
+    <>
+      <div>
+        <Select options={options} />
+        <ul>
+          {salons && salons.length > 0 ? (
+            salons.map((salon) => renderSalon(salon))
+          ) : (
+            <p>No salons found</p>
+          )}
+        </ul>
+      </div>
+    </>
   );
 }
 
