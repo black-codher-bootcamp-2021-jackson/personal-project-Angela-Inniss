40ab16a75114bee52520e9eab61d019872d793bc
Angela-Inniss <angela.inniss@hotmail.co.uk>
2022-02-06 16:45:25 +0000

    remove custom styles

--- client/src/components/SelectDropdown/SelectDropdown.js
+++ client/src/components/SelectDropdown/SelectDropdown.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import Select from 'react-select'
 
-
-// const customStyles = {
-//     option: (provided, state) => ({
-//         ...provided,
-//         borderBottom: '1px dotted pink',
-//         color: state.isSelected ? 'red' : 'blue',
-//         padding: 20,
-//     }),
-//     control: () => ({
-//         // none of react-select's styles are passed to <Control />
-//         width: 200,
-//     }),
-//     singleValue: (provided, state) => {
-//         const opacity = state.isDisabled ? 0.5 : 1;
-//         const transition = 'opacity 300ms';
-
-//         return { ...provided, opacity, transition };
-//     }
-// }
 const SelectDropDownFilter = (props) => (
     <Select isMulti={props.isMulti} placeholder={props.placeholder} options={props.options} onChange={props.onChange} />
 )
