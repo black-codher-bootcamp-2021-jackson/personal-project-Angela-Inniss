6f89122aba6ee23cffd5e4726dc8819a144e20f0
Angela-Inniss <angela.inniss@hotmail.co.uk>
2022-02-08 20:43:15 +0000

    fix routing for filtering add homepage styles

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,34 @@
 import React from "react";
 import SalonCard from "../../components/SalonCard/SalonCard";
+import "./home.css"
+import texture from "../../images/textureBG.png";
+import homegirl from "../../images/GirlHome.jpg"
 
 const Home = () => {
     return (
-        <SalonCard
-            image="test"
-            name="Snippers"
-            location="Manchester"
-            socials={[]}
-            description="this is a description of the salon"
-            services={[]} />
+        <>
+            <nav className="navbar">
+                <div className="nav-item">About</div>
+                <div className="nav-item" >Be Insipired</div>
+                <div className="nav-item">Search</div>
+            </nav>
+            <div className="container">
+                <div className="background-white">
+                    <div>
+                        <h1>Salon</h1>
+                        <h1>Search</h1>
+                        <h3>this is a subheading with text about webiste </h3>
+                    </div>
+                </div>
+                <div className="background-image">
+                    <img src={texture}></img>
+
+                </div>
+            </div>
+            <img className="homepage-girl" src={homegirl}></img>
+            <h1 className="sub-title">Find Inspo</h1>
+            <div>subtext</div>
+        </>
     )
 }
 export default Home;
