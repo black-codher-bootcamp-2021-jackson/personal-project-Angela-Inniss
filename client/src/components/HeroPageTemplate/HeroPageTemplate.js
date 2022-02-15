25230df5a480f519cb98fc917f60eb2efc2510d3
Angela-Inniss <angela.inniss@hotmail.co.uk>
2022-02-15 23:26:14 +0000

    Add hero template

--- client/src/components/HeroPageTemplate/HeroPageTemplate.js
+++ client/src/components/HeroPageTemplate/HeroPageTemplate.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import texture from "../../images/textureBG.png";
-import homegirl from "../../images/GirlHome.jpg"
+import homegirl from "../../images/GirlHome.jpg";
+import girl1 from "../../images/girl1.jpg";
+import girl2 from "../../images/girl2.jpg"
+import girl3 from "../../images/girl3.jpg"
+import girl4 from "../../images/girl4.jpg";
+import girl5 from "../../images/girl5.jpg"
 
 import "./heroPageTemplate.css";
 
@@ -33,19 +38,19 @@ const HeroPageTemplate = (props) => {
                     <img src={homegirl} />
                 </div>
                 <div className="inspo-item">
-                    <img src={homegirl} />
+                    <img src={girl1} />
                 </div>
                 <div className="inspo-item">
-                    <img src={homegirl} />
+                    <img src={girl2} />
                 </div>
                 <div className="inspo-item">
-                    <img src={homegirl} />
+                    <img src={girl3} />
                 </div>
                 <div className="inspo-item">
-                    <img src={homegirl} />
+                    <img src={girl4} />
                 </div>
                 <div className="inspo-item">
-                    <img src={homegirl} />
+                    <img src={girl5} />
                 </div>
             </div>
 
