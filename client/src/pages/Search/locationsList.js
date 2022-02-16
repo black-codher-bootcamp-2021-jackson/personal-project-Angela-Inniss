c730b122f9217cec502a54fcc1047dd9c873659a
Angela-Inniss <angela.inniss@hotmail.co.uk>
2022-02-16 18:14:26 +0000

    commit before gets messy playing with filters

---
+++ client/src/pages/Search/locationsList.js
@@ -0,0 +1,17 @@
+export const locationsList = [
+    {
+        value: "London",
+        label: "London"
+
+    },
+    {
+        value: "Manchester",
+        label: "Manchester"
+
+    },
+    {
+        value: "Birmingham",
+        label: "Birmingham"
+
+    },
+]
